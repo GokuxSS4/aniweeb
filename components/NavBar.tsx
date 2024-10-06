f830fa19c580a45a8f7c6ce81d5a6feb2259747c
jaykumar286 <jaykumarchavda2806@gmail.com>
2024-10-06 14:47:11 +0530

    modified the navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,44 +4,19 @@ import { CiSearch } from "react-icons/ci";
 
 export function NavBar() {
   return (
-    <nav className="w-11/12 mx-auto bg-transparent h-16 flex items-center justify-around absolute inset-0 z-50">
-      <h1 className="">Aniweeb.tv</h1>
+    <nav className="w-full bg-transparent h-16 absolute z-50">
+      <div className="mx-auto flex items-center justify-between py-4 px-12">
+        <h1 className="">Aniweeb.tv</h1>
 
-      <div className="flex items-center gap-5">
-        <Link
-          href="#"
-          className="px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700 hover:text-white"
-        >
-          Home
-        </Link>
-        <Link
-          href="#"
-          className="px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700 hover:text-white"
-        >
-          Trending
-        </Link>
-        <Link
-          href="#"
-          className="px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700 hover:text-white"
-        >
-          Popular
-        </Link>
-        <Link
-          href="#"
-          className="px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700 hover:text-white"
-        >
-          Schedule
-        </Link>
-      </div>
-
-      <div className="relative">
-        <input
-          type="text"
-          placeholder="Search anime..."
-          className="bg-slate-200 text-white pl-10 pr-4 py-2 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-600"
-        />
-        <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-          <CiSearch className="h-5 w-5 text-gray-400" />
+        <div className="relative">
+          <input
+            type="text"
+            placeholder="Search anime..."
+            className="bg-slate-200 text-white pl-10 pr-4 py-2 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-600"
+          />
+          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+            <CiSearch className="h-5 w-5 text-gray-400" />
+          </div>
         </div>
       </div>
     </nav>
