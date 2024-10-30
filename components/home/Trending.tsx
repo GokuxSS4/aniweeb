eb1d3c0cbfdc661791a49a703a76bbc72048c0ef
jaykumar286 <jaykumarchavda2806@gmail.com>
2024-10-30 17:30:44 +0530

    make trending component responsive

--- components/home/Trending.tsx
+++ components/home/Trending.tsx
@@ -9,33 +9,35 @@ import { Header } from "@/components/common/Header";
 
 function TrendingAnimeCard({ anime }: { anime: HiAnime.TrendingAnime }) {
   return (
-    <div className="transform transition-transform duration-500 hover:-translate-y-2 group">
-      <div className="w-[calc(16.66% - 1rem)] gap-2 p-2 flex flex-col flex-shrink-0">
-        <div className="w-full aspect-[2/3] overflow-hidden relative">
-          <img
-            src={anime.poster || ""}
-            alt={anime.name || "failed to retrive image"}
-            className="rounded-md h-full w-full object-cover"
-            loading="lazy"
-          />
-          <div className="absolute top-1 left-1">
-            <div className="bg-black/60 backdrop-blur-sm px-2 py-1 rounded-md shadow-lg">
-              <span className="text-white font-semibold text-sm">
-                #{anime.rank}
-              </span>
-            </div>
+    <div className="w-[calc(16.66% - 1rem)] gap-2 p-2 flex flex-col flex-shrink-0">
+      <div className="w-full aspect-[2/3] overflow-hidden relative">
+        <img
+          src={anime.poster || ""}
+          alt={anime.name || "failed to retrive image"}
+          className="rounded-md h-full w-full object-cover"
+          loading="lazy"
+        />
+        <div className="absolute top-0 left-0">
+          <div className="relative flex items-center justify-center size-8 lg:size-10">
+            <div className="absolute inset-0 bg-black/30 backdrop-blur-md" />
+
+            <div className="absolute inset-0 border-2 border-white/30" />
+
+            <span className="relative text-white text-sm md:text-base lg:text-lg font-bold">
+              #{anime.rank}
+            </span>
           </div>
         </div>
-
-        <p className="line-clamp-1 text-white text-sm">{anime.name}</p>
       </div>
+
+      <p className="line-clamp-1 text-white text-sm">{anime.name}</p>
     </div>
   );
 }
 
-const TrendingCardSkeleton = () => {
+function TrendingCardSkeleton() {
   return (
-    <div className="min-w-[16.66%] px-2">
+    <div className="px-2 w-full">
       <div className="transform transition-transform duration-500 hover:cursor-pointer">
         <div className="w-full aspect-[2/3] overflow-hidden relative">
           <div className="h-full w-full rounded-md bg-[#141414] animate-pulse" />
@@ -43,126 +45,129 @@ const TrendingCardSkeleton = () => {
       </div>
     </div>
   );
-};
-
-function LoadingCards({ noOfCards }: { noOfCards: number }) {
-  console.log("Loading cards", noOfCards);
-  return (
-    <div className="flex h-full">
-      {Array.from({ length: noOfCards }).map((_, index) => (
-        <TrendingCardSkeleton key={index} />
-      ))}
-    </div>
-  );
 }
 
-export function Trending({ aniList }: { aniList: HiAnime.TrendingAnime[] }) {
-  const sliderRef = useRef<Slider>(null);
-
-  const next = () => {
-    if (sliderRef && sliderRef.current) {
-      sliderRef.current.slickNext();
-    }
-  };
-
-  const previous = () => {
-    if (sliderRef && sliderRef.current) {
-      sliderRef.current.slickPrev();
-    }
-  };
-
-  const [isLoading, setIsloading] = useState(true);
-
-  const [currentSlide, setCurrentSlide] = useState(0);
-  const [slidesToShow, setSlidesToShow] = useState(6);
-
-  const updateSlidesToShow = () => {
-    const width = window.innerWidth;
-
-    if (width < 500) {
-      setSlidesToShow(1);
-    } else if (width < 700) {
-      setSlidesToShow(2);
-    } else if (width < 900) {
-      setSlidesToShow(3);
-    } else if (width < 1100) {
-      setSlidesToShow(4);
-    } else if (width < 1356) {
-      setSlidesToShow(5);
-    } else {
-      setSlidesToShow(6);
-    }
-  };
-
-  useEffect(() => {
-    updateSlidesToShow();
-
-    const handleResize = () => {
-      updateSlidesToShow();
-    };
-
-    window.addEventListener("resize", handleResize);
-    setIsloading(false);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  const settings = {
-    dots: false,
-    infinite: false,
-    speed: 500,
-    slidesToShow: slidesToShow,
-    slidesToScroll: 1,
-    arrows: false,
-    swipeToSlide: true,
-    beforeChange: (current: any, next: any) => {
-      setCurrentSlide(next);
-    },
-  };
-
-  const numItems = aniList.length;
-  const showPrevArrow = currentSlide > 0;
-  const showNextArrow = currentSlide + settings.slidesToShow < numItems;
-
+function LoadingSkeletons() {
   return (
-    <div className="w-full h-full">
-      <Header title={"Trending"} />
-      <div className="slider-container relative">
-        <div className="w-full px-8 relative">
-          {isLoading ? (
-            <LoadingCards noOfCards={slidesToShow} />
-          ) : (
-            <>
-              <Slider ref={sliderRef} {...settings}>
-                {aniList.map((anime: HiAnime.TrendingAnime) => (
-                  <TrendingAnimeCard anime={anime} key={anime.id} />
-                ))}
-              </Slider>
-
-              <button
-                onClick={previous}
-                disabled={!showPrevArrow}
-                className={`absolute left-0 top-[45%] bg-opacity-75 transform -translate-y-1/2 rounded-full p-3 text-white flex items-center justify-center transition-all duration-200 
-              ${!showPrevArrow && "invisible"}`}
-              >
-                <FaChevronLeft size={24} />
-              </button>
-
-              <button
-                onClick={next}
-                disabled={!showNextArrow}
-                className={`absolute right-0 top-[45%] bg-opacity-75 transform -translate-y-1/2 rounded-full p-3 text-white flex items-center justify-center transition-all duration-200
-              ${!showNextArrow && "invisible"}`}
-              >
-                <FaChevronRight size={24} />
-              </button>
-            </>
-          )}
-        </div>
+    <div className="overflow-hidden">
+      <div className="flex gap-4 w-full">
+        {[...Array(6)].map((_, index) => (
+          <div
+            key={index}
+            className={`
+              flex-1 min-w-0
+              ${index == 3 ? "hidden md:block" : ""}
+              ${index == 4 ? "hidden xl:block" : ""}
+              ${index == 5 ? "hidden 2xl:block" : ""}
+            `}
+          >
+            <TrendingCardSkeleton />
+          </div>
+        ))}
       </div>
     </div>
   );
 }
 
+interface TrendingProps {
+  aniList: HiAnime.TrendingAnime[];
+}
+
+export function Trending({ aniList }: TrendingProps) {
+  const sliderRef = useRef<Slider>(null);
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  const next = () => {
+    sliderRef.current?.slickNext();
+  };
+
+  const previous = () => {
+    sliderRef.current?.slickPrev();
+  };
+
+  const settings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    arrows: false,
+    swipeToSlide: true,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1411,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    <div className="w-full h-full">
+      <Header title="Trending" />
+      <div className="relative w-full px-0 md:px-8">
+        {isMounted ? (
+          <>
+            <Slider ref={sliderRef} {...settings} className="!static">
+              {isMounted
+                ? aniList.map((anime) => (
+                    <TrendingAnimeCard anime={anime} key={anime.id} />
+                  ))
+                : Array.from({ length: aniList.length }).map((_, index) => (
+                    <TrendingCardSkeleton key={index} />
+                  ))}
+            </Slider>
+
+            <button
+              onClick={previous}
+              className="absolute left-0 top-[45%] bg-opacity-75 transform -translate-y-1/2 rounded-full p-3 text-white  hidden md:flex items-center justify-center transition-all duration-200 hover:bg-white/10"
+            >
+              <FaChevronLeft size={24} />
+            </button>
+
+            <button
+              onClick={next}
+              className="absolute right-0 top-[45%] bg-opacity-75 transform -translate-y-1/2 rounded-full p-3 text-white  hidden md:flex items-center justify-center transition-all duration-200 hover:bg-white/10"
+            >
+              <FaChevronRight size={24} />
+            </button>
+          </>
+        ) : (
+          <LoadingSkeletons />
+        )}
+      </div>
+    </div>
+  );
+}
