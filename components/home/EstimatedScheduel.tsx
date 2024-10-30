05463b5660d47d191a6492d5feb9f65775ee5477
jaykumar286 <jaykumarchavda2806@gmail.com>
2024-10-30 19:24:45 +0530

    added an skeleton for estimated scheduel

--- components/home/EstimatedScheduel.tsx
+++ components/home/EstimatedScheduel.tsx
@@ -40,19 +40,52 @@ function getFormattedDate(date: Date) {
   };
 }
 
+function WeekItem({
+  date,
+  isSelected,
+  onSelect,
+}: {
+  date: Date;
+  isSelected: boolean;
+  onSelect: (date: Date) => void;
+}) {
+  const { day, month, dayOfWeek } = getFormattedDate(date);
+
+  return (
+    <button
+      className={`flex grow flex-col px-4 py-2 bg-[#0f0f11] items-center justify-center rounded-md ${
+        isSelected ? "bg-primary text-black" : ""
+      }`}
+      onClick={() => onSelect(date)}
+    >
+      <h3 className="font-bold">{dayOfWeek}</h3>
+      <div
+        className={`flex gap-2 text-sm ${
+          isSelected ? "text-black" : "text-gray-400"
+        }`}
+      >
+        <p>{month}</p>
+        <p>{day}</p>
+      </div>
+    </button>
+  );
+}
+
 function ScheduleItem({
   schedule,
   index,
-  isExpanded
+  isExpanded,
 }: {
   schedule: EstimatedSchedule;
   index: number;
-  isExpanded:boolean
+  isExpanded: boolean;
 }) {
   return (
     <div
       key={schedule.id}
-      className={`border-b border-white-10 py-3 justify-between brightness-75 hover:brightness-100 cursor-pointer ${index >= (5) && !isExpanded ? 'hidden' : 'flex'}`}
+      className={`border-b border-white-10 py-3 justify-between brightness-75 hover:brightness-100 cursor-pointer ${
+        index >= 5 && !isExpanded ? "hidden" : "flex"
+      }`}
     >
       <div className="flex gap-4">
         <p className="font-light">{schedule.time}</p>
@@ -67,16 +100,30 @@ function ScheduleItem({
   );
 }
 
+function WeekItemSkeleton() {
+  return (
+    <div className="flex grow h-16 flex-col px-4 py-2 items-center justify-center rounded-md animate-pulse transform transition-transform duration-500 bg-gray-700"></div>
+  );
+}
+
+function ScheduleItemSkeleton() {
+  return (
+    <div className="py-3  w-full h-8 flex justify-between brightness-75 animate-pulse transform transition-transform duration-500 hover:cursor-pointer bg-gray-700 rounded"></div>
+  );
+}
+
 export function EstimatedSchedule() {
   const [weekList, setWeekList] = useState<Date[]>([]);
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [scheduleList, setScheduelList] = useState<EstimatedSchedule[]>([]);
 
   const [isExpanded, setIsExpanded] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
     const week = getWeekByDate(new Date());
     setWeekList(week);
+    setIsMounted(true);
   }, []);
 
   useEffect(() => {
@@ -91,35 +138,43 @@ export function EstimatedSchedule() {
     <div className="w-full h-full">
       <Header title={"Estimated Schedule"} />
       <div className="grid grid-cols-4  md:flex gap-3 my-4 mb-6 flex-wrap">
-        {weekList.map((date: Date, index: number) => {
-          const { day, month, dayOfWeek } = getFormattedDate(date);
-          const isSelectedDate = day === getFormattedDate(selectedDate).day;
-          return (
-            <button
-              key={index}
-              className={`flex grow flex-col px-4 py-2 bg-[#0f0f11] items-center justify-center rounded-md ${
-                isSelectedDate && "bg-primary text-black"
-              }`}
-              onClick={() => setSelectedDate(date)}
-            >
-              <h3 className="font-bold">{dayOfWeek}</h3>
-              <div
-                className={`flex gap-2 text-sm ${
-                  isSelectedDate ? " text-black" : "text-gray-400"
-                }`}
-              >
-                <p>{month}</p>
-                <p>{day}</p>
-              </div>
-            </button>
-          );
-        })}
+        {isMounted
+          ? weekList.map((date: Date, index: number) => {
+              const { day } = getFormattedDate(date);
+              const isSelectedDate = day === getFormattedDate(selectedDate).day;
+              return (
+                <WeekItem
+                  key={index}
+                  date={date}
+                  isSelected={isSelectedDate}
+                  onSelect={setSelectedDate}
+                />
+              );
+            })
+          : Array.from({ length: 7 }).map((_, index) => (
+              <WeekItemSkeleton key={index} />
+            ))}
       </div>
 
       <div className="flex flex-col gap-4 ">
-        {scheduleList.map((schedule: EstimatedSchedule, index: number) => <ScheduleItem key={schedule.id} schedule={schedule} index={index} isExpanded={isExpanded} /> )}
+        {isMounted
+          ? scheduleList.map((schedule: EstimatedSchedule, index: number) => (
+              <ScheduleItem
+                key={schedule.id}
+                schedule={schedule}
+                index={index}
+                isExpanded={isExpanded}
+              />
+            ))
+          : Array.from({ length: 5 }).map((_, index) => (
+              <ScheduleItemSkeleton key={index} />
+            ))}
       </div>
-      <div className={`w-full justify-end mt-6 ${scheduleList.length > 5 ? 'flex':'hidden'}`}>
+      <div
+        className={`w-full justify-end mt-6 ${
+          scheduleList.length > 5 ? "flex" : "hidden"
+        }`}
+      >
         <button
           className="flex justify-center items-center gap-1 text-white brightness-75 hover:brightness-100"
           onClick={() => setIsExpanded(!isExpanded)}
