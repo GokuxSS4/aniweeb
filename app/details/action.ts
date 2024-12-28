2d65ce304d53db52b19d08cb65e6902dc0764c0f
jaykumar286 <jaykumarchavda2806@gmail.com>
2024-12-28 16:34:29 +0530

    feat(docker):added an docker compose

--- app/details/action.ts
+++ app/details/action.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { aniScraper } from "@/config/aniScraper";
-// import { redis } from "@/config/redis";
+import { redis } from "@/config/redis";
 
 const ANIME_DETAILS = "details";
 
@@ -11,15 +11,16 @@ const EXPIRY_MS = `PX`;
 export async function getAnimeDetails(animeId: string) {
   const key = `${ANIME_DETAILS}_${animeId}`;
 
-  // const cachedHomeData = await redis.get(key);
+  const cachedHomeData = await redis.get(key);
 
-  // if (cachedHomeData) {
-  //   return JSON.parse(cachedHomeData);
-  // }
+  if (cachedHomeData) {
+    console.log("Redis cache data",cachedHomeData)
+    return JSON.parse(cachedHomeData);
+  }
 
   const animeDetails = await aniScraper.getInfo(animeId);
 
-  // await redis.set(key, JSON.stringify(animeDetails), EXPIRY_MS, MAX_AGE);
+  await redis.set(key, JSON.stringify(animeDetails), EXPIRY_MS, MAX_AGE);
 
   return animeDetails;
 }
--- config/redis.ts
+++ config/redis.ts
@@ -1,6 +1,11 @@
 import Redis from 'ioredis';
 
+const redisHost = process.env.REDIS_HOST || 'localhost';
+const redisPort = parseInt(process.env.REDIS_PORT || '6379', 10);
+
+console.log("Redis Port:", redisPort, "Type:", typeof redisPort);
+
 export const redis = new Redis({
-  host:'redis',
-  port:6379
+  host: redisHost,
+  port: redisPort,
 });
