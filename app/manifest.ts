654e3531e144c31fc84385590e5eabaeed52d927
jaykumar286 <jaykumarchavda2806@gmail.com>
2024-12-31 20:47:36 +0530

    added an lint and format stage in ci

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,24 +1,24 @@
-import type { MetadataRoute } from 'next';
- 
+import type { MetadataRoute } from "next";
+
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'Aniweeb',
-    short_name: 'Aniweeb',
-    description: 'Free Anime Streaming Platform',
-    start_url: '/',
-    display: 'standalone',
-    background_color: '#2EC6FE',
-    theme_color: '#8936FF',
+    name: "Aniweeb",
+    short_name: "Aniweeb",
+    description: "Free Anime Streaming Platform",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#2EC6FE",
+    theme_color: "#8936FF",
     icons: [
       {
         src: "/icons/aniweeb_maskable.png",
         sizes: "512x512",
-        type: 'image/png',
+        type: "image/png",
       },
       {
         src: "/icons/aniweeb_maskable.png",
         sizes: "512x512",
-        type: 'image/png',
+        type: "image/png",
       },
     ],
   };
