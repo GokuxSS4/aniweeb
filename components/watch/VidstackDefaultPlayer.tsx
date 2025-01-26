07d2300ca2840999ddd7c1ad0d9123b31c3031de
jaykumar286 <jaykumarchavda2806@gmail.com>
2025-01-26 18:14:46 +0530

    fix the seek forward and backward for video player

--- components/watch/VidstackDefaultPlayer.tsx
+++ components/watch/VidstackDefaultPlayer.tsx
@@ -63,15 +63,26 @@ export function VidstackDefaultPlayer({
       <DefaultAudioLayout icons={defaultLayoutIcons} />
       <DefaultVideoLayout
         icons={defaultLayoutIcons}
+        noGestures={false}
         slots={{
+          afterCaptionButton: (
+            <SeekButton seconds={-10} className="block md:hidden vds-button">
+              <SeekBackward10Icon size={32} className="vds-icon" />
+            </SeekButton>
+          ),
+          beforeSettingsMenu: (
+            <SeekButton seconds={10} className="block md:hidden vds-button">
+              <SeekForward10Icon size={32} className="vds-icon" />
+            </SeekButton>
+          ),
           afterPlayButton: (
-            <SeekButton seconds={-10} className="vds-button">
-              <SeekBackward10Icon className="vds-icon" />
+            <SeekButton seconds={-10} className="hidden md:block vds-button">
+              <SeekBackward10Icon size={32} className="vds-icon" />
             </SeekButton>
           ),
           beforeMuteButton: (
-            <SeekButton seconds={10} className="vds-button">
-              <SeekForward10Icon className="vds-icon" />
+            <SeekButton seconds={10} className="hidden md:block vds-button">
+              <SeekForward10Icon size={32} className="vds-icon" />
             </SeekButton>
           ),
         }}
